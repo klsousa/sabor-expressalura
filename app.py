8c7206db376300caeeaf5a4d982d288b9a755f71
Primeira aula <ks796198@gmail.com>
2024-05-03 07:53:07 -0300

    Manipulação de Strings

---
+++ app.py
@@ -0,0 +1,73 @@
+print("""""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida =  input('Escolha uma opção: ')
+print(f'Voce escolheu a opção: {opcao_escolhida}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#APRENDIZADO
+""" print('Python na Escola de Programação da Alura')
+nome = 'Kelvin'
+idade = 23
+print(f'Meu nome e {nome} e tenho {idade} anos')
+print('A','L','U','R','A',sep='\n')
+
+pi = 3.14159
+print(f'O valor arredondado de pi é: {pi}')
+
+
+pi = 3.14159
+
+# Abordagem de f-string
+print(f'O valor arredondado de pi é: {pi:.2f}')
+
+# Abordagem de .format()
+print('O valor arredondado de pi é: {:.2f}'.format(pi))
+
+# Utilizando a função round()
+print('O valor arredondado de pi é:', round(pi, 2))
+
+# Criação das Variáveis
+nome = 'Lais'
+idade = 24
+
+# Abordagem mais simples
+print('Meu nome é',nome,'e tenho',idade,'anos.')
+
+# Abordagem do f-string
+print(f'Meu nome é {nome} e tenho {idade} anos.')
+
+# Abordagem do .format()
+print('Meu nome é {} e tenho {} anos.'.format(nome,idade))
+
+# Abordagem da % (Formatação de String Antiga - Python 2)
+print('Meu nome é %s e tenho %i anos.'%(nome,idade)) """
+
